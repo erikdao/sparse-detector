ceaa3a58a35fc66263e5ee41f0f845821fd60079
erikdao <cuong@neuraltalks.io>
2022-03-10 01:07:05 +0100

    Remove unused texts

--- sparse_detector/datasets/transforms.py
+++ sparse_detector/datasets/transforms.py
@@ -1,4 +1,3 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 """
 Transforms and data augmentation for both image + bbox.
 """
@@ -9,8 +8,8 @@ import torch
 import torchvision.transforms as T
 import torchvision.transforms.functional as F
 
-from util.box_ops import box_xyxy_to_cxcywh
-from util.misc import interpolate
+from sparse_detector.util.box_ops import box_xyxy_to_cxcywh
+from sparse_detector.util.misc import interpolate
 
 
 def crop(image, target, region):
@@ -34,11 +33,6 @@ def crop(image, target, region):
         target["boxes"] = cropped_boxes.reshape(-1, 4)
         target["area"] = area
         fields.append("boxes")
-
-    if "masks" in target:
-        # FIXME should we update the area here if there are no boxes?
-        target['masks'] = target['masks'][:, i:i + h, j:j + w]
-        fields.append("masks")
 
     # remove elements for which the boxes or masks that have zero area
     if "boxes" in target or "masks" in target:
@@ -66,9 +60,6 @@ def hflip(image, target):
         boxes = target["boxes"]
         boxes = boxes[:, [2, 1, 0, 3]] * torch.as_tensor([-1, 1, -1, 1]) + torch.as_tensor([w, 0, w, 0])
         target["boxes"] = boxes
-
-    if "masks" in target:
-        target['masks'] = target['masks'].flip(-1)
 
     return flipped_image, target
 
@@ -125,10 +116,6 @@ def resize(image, target, size, max_size=None):
     h, w = size
     target["size"] = torch.tensor([h, w])
 
-    if "masks" in target:
-        target['masks'] = interpolate(
-            target['masks'][:, None].float(), size, mode="nearest")[:, 0] > 0.5
-
     return rescaled_image, target
 
 
@@ -140,8 +127,6 @@ def pad(image, target, padding):
     target = target.copy()
     # should we do something wrt the original size?
     target["size"] = torch.tensor(padded_image.size[::-1])
-    if "masks" in target:
-        target['masks'] = torch.nn.functional.pad(target['masks'], (0, padding[0], 0, padding[1]))
     return padded_image, target
 
 
