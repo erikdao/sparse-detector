36cf0f248c8f756689d2ce59e684a1c28a578d91
erikdao <cuong@neuraltalks.io>
2022-03-18 19:13:20 +0100

    Add: visualize detection results

--- sparse_detector/models/utils.py
+++ sparse_detector/models/utils.py
@@ -4,6 +4,12 @@ Model's utilities
 from tabulate import tabulate
 
 
+def _get_top_level_module_name(model):
+    names = [name for name, _ in model.named_modules()]
+    prefixes = [name.split(".")[0] for name in names if name]
+    return set(prefixes)
+
+
 def count_module_params(module):
     trainable = sum(p.numel() for p in module.parameters() if p.requires_grad)
     non_trainable = sum(p.numel() for p in module.parameters() if p.requires_grad is False)
@@ -11,7 +17,9 @@ def count_module_params(module):
     return trainable, non_trainable
 
 
-def describe_model(model, keys=("backbone", "input_proj", "query_embed", "class_embed", "bbox_embed", "transformer")):
+def describe_model(model):
+    keys = _get_top_level_module_name(model)
+    
     data = [("Module", "Trainable", "Non-trainable")]
     trainable_sum, non_trainable_sum = 0, 0
 
@@ -21,6 +29,7 @@ def describe_model(model, keys=("backbone", "input_proj", "query_embed", "class_
         data.append((key, trainable, non_trainable))
         trainable_sum += trainable
         non_trainable_sum += non_trainable
+
     data.append(("", None, None))
     data.append(("Total", trainable_sum, non_trainable_sum))
     print(tabulate(data, headers="firstrow", tablefmt="psql"))
