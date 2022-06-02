9a5d71c18d447ae6977ba3148e93a78edd1f79c1
erikdao <cuong@neuraltalks.io>
2022-06-02 09:27:19 +0200

    WIP: metrics analyses for sparsemax and entmax15

--- scripts/run_compute_metrics.sh
+++ scripts/run_compute_metrics.sh
@@ -5,16 +5,24 @@ PROJ_DIR=/proj/azizpour-group/users/cuongdao/projects/sparse-detector
 SCRIPT_DIR=$PROJ_DIR/scripts
 SEED=42
 
-metricthreshold=(1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10)
+# metricthreshold=(1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10)
 
-for m in "${metricthreshold[@]}"
-do
-    echo "Computing metric with threshold $m"
-    torchrun --nproc_per_node=8 $SCRIPT_DIR/compute_val_metrics.py zeros_ratio \
-        --detr-config-file "configs/detr_baseline.yml" \
+# for m in "${metricthreshold[@]}"
+# do
+#     echo "Computing metric with threshold $m"
+#     torchrun --nproc_per_node=8 $SCRIPT_DIR/compute_val_metrics.py zeros_ratio \
+#         --detr-config-file "configs/detr_baseline.yml" \
+#         --coco-path /proj/azizpour-group/users/cuongdao/data/COCO \
+#         --batch-size 6 \
+#         --num-workers 24 \
+#         --decoder-act softmax \
+#         --metric-threshold $m
+# done
+
+torchrun --nproc_per_node=8 $SCRIPT_DIR/compute_val_metrics.py zeros_ratio \
+        --detr-config-file "configs/decoder_entmax15_baseline.yml" \
         --coco-path /proj/azizpour-group/users/cuongdao/data/COCO \
         --batch-size 6 \
         --num-workers 24 \
-        --decoder-act softmax \
-        --metric-threshold $m
-done
+        --decoder-act entmax15 \
+        --resume-from-checkpoint "checkpoints/v2_decoder_entmax15/checkpoint.pth"
