85029f38eca434d773ab51abb9dd3291e804b51c
erikdao <cuong@neuraltalks.io>
2022-06-05 00:03:44 +0200

    Add: visualize predicted and groundtruth matched bboxes

--- scripts/compute_val_metrics.py
+++ scripts/compute_val_metrics.py
@@ -10,6 +10,7 @@ import time
 import random
 import datetime
 import warnings
+
 warnings.filterwarnings("ignore")
 
 package_root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -18,49 +19,75 @@ sys.path.insert(0, package_root)
 import click
 import torch
 import numpy as np
-from tqdm import tqdm
 
 from sparse_detector.models import build_model
 from sparse_detector.models.utils import describe_model
-from sparse_detector.utils.metrics import gini_vectorized, zeros_ratio_vectorized, paibb_vectorized
-from sparse_detector.utils import distributed  as dist_utils
-from sparse_detector.configs import build_detr_config, build_matcher_config, load_base_configs, build_dataset_config
+from sparse_detector.utils.metrics import (
+    gini_vectorized,
+    zeros_ratio_vectorized,
+    paibb_vectorized,
+)
+from sparse_detector.utils import distributed as dist_utils
+from sparse_detector.configs import (
+    build_detr_config,
+    build_matcher_config,
+    load_base_configs,
+    build_dataset_config,
+)
 from sparse_detector.datasets.loaders import build_dataloaders
 from sparse_detector.utils.logging import MetricLogger
 from sparse_detector.models.matcher import build_matcher
 
 
 @click.command()
-@click.argument('metric', default='gini', type=str)
-@click.option('--metric-threshold', type=float, default=None, help='Threshold for metric')
-@click.option('--seed', type=int, default=42)
-@click.option('--detr-config-file', default='', help="Path to config file")
-@click.option('--decoder-act', type=str, default='sparsemax')
-@click.option('--coco-path', type=str, default="./data/COCO")
-@click.option('--num-workers', default=12, type=int)
-@click.option('--batch-size', default=6, type=int, help="Batch size per GPU")
-@click.option('--resume-from-checkpoint', default='', help='resume from checkpoint')
-@click.option('--detection-threshold', default=None, type=float, help='Threshold to filter detection results')
-@click.option('--pre-norm/--no-pre-norm', default=True)
+@click.argument("metric", default="gini", type=str)
+@click.option(
+    "--metric-threshold", type=float, default=None, help="Threshold for metric"
+)
+@click.option("--seed", type=int, default=42)
+@click.option("--detr-config-file", default="", help="Path to config file")
+@click.option("--decoder-act", type=str, default="sparsemax")
+@click.option("--coco-path", type=str, default="./data/COCO")
+@click.option("--num-workers", default=12, type=int)
+@click.option("--batch-size", default=6, type=int, help="Batch size per GPU")
+@click.option("--resume-from-checkpoint", default="", help="resume from checkpoint")
+@click.option(
+    "--detection-threshold",
+    default=None,
+    type=float,
+    help="Threshold to filter detection results",
+)
+@click.option("--pre-norm/--no-pre-norm", default=True)
 @click.pass_context
 def main(
-    ctx, metric, detr_config_file, resume_from_checkpoint, seed,
-    decoder_act, pre_norm, coco_path, num_workers, batch_size, detection_threshold,
-    metric_threshold
+    ctx,
+    metric,
+    detr_config_file,
+    resume_from_checkpoint,
+    seed,
+    decoder_act,
+    pre_norm,
+    coco_path,
+    num_workers,
+    batch_size,
+    detection_threshold,
+    metric_threshold,
 ):
-    if metric not in ['gini', 'zeros_ratio', 'paibb']:
+    if metric not in ["gini", "zeros_ratio", "paibb"]:
         raise ValueError(f"Metric {metric} not supported! Quitting!")
 
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     base_config = load_base_configs()
-    dist_config = dist_utils.init_distributed_mode(base_config['distributed']['dist_url'])
+    dist_config = dist_utils.init_distributed_mode(
+        base_config["distributed"]["dist_url"]
+    )
     detr_config = build_detr_config(detr_config_file, params=ctx.params, device=device)
-    dataset_config = build_dataset_config(base_config['dataset'], params=ctx.params)
+    dataset_config = build_dataset_config(base_config["dataset"], params=ctx.params)
 
     # By default the attention weights are averaged across heads. However for computing the gini scores
     # It is better to compute the score on each attention weight matrix for each head separately. This
     # is to avoid the zeros being destroyed through the average.
-    detr_config['average_cross_attn_weights'] = False
+    detr_config["average_cross_attn_weights"] = False
     print(detr_config)
 
     # Fix the see for reproducibility
@@ -75,30 +102,37 @@ def main(
     if resume_from_checkpoint:
         print(f"Load model from checkpoint: {resume_from_checkpoint}")
         checkpoint = torch.load(resume_from_checkpoint, map_location="cpu")
-        model.load_state_dict(checkpoint['model'])
+        model.load_state_dict(checkpoint["model"])
     model.eval()
     model.to(device)
     describe_model(model)
     criterion.eval()
 
     if dist_config.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[dist_config.gpu])
+        model = torch.nn.parallel.DistributedDataParallel(
+            model, device_ids=[dist_config.gpu]
+        )
 
     matcher = None
-    if metric == 'paibb':
-        print("Building matcher for metric: Percentage of Attentions Inside Bounding Boxes")
+    if metric == "paibb":
+        print(
+            "Building matcher for metric: Percentage of Attentions Inside Bounding Boxes"
+        )
         matcher_config = build_matcher_config(detr_config_file)
         print(matcher_config)
         matcher = build_matcher(
-            set_cost_class=matcher_config['set_cost_class'],
-            set_cost_bbox=matcher_config['set_cost_bbox'],
-            set_cost_giou=matcher_config['set_cost_giou']
+            set_cost_class=matcher_config["set_cost_class"],
+            set_cost_bbox=matcher_config["set_cost_bbox"],
+            set_cost_giou=matcher_config["set_cost_giou"],
         )
-    
+
     print("Building dataset")
     data_loader, _ = build_dataloaders(
-        'val', dataset_config['coco_path'], dataset_config['batch_size'],
-        dist_config.distributed, dataset_config['num_workers']
+        "val",
+        dataset_config["coco_path"],
+        dataset_config["batch_size"],
+        dist_config.distributed,
+        dataset_config["num_workers"],
     )
 
     print(f"Computing {metric}")
@@ -107,13 +141,19 @@ def main(
 
     start_time = time.time()
     dataset_metric = []
-    for batch_id, (samples, targets) in enumerate(metric_logger.log_every(data_loader, log_freq=10, header=header, prefix="val")):
+    for batch_id, (samples, targets) in enumerate(
+        metric_logger.log_every(data_loader, log_freq=10, header=header, prefix="val")
+    ):
         samples = samples.to(device)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
         attentions = []
         conv_features = []
-        hooks = [model.module.backbone[-2].register_forward_hook(lambda self, input, output: conv_features.append(output)),]
+        hooks = [
+            model.module.backbone[-2].register_forward_hook(
+                lambda self, input, output: conv_features.append(output)
+            ),
+        ]
 
         for i in range(6):
             hooks.append(
@@ -127,20 +167,22 @@ def main(
             hook.remove()
 
         batch_attns = torch.stack(attentions)  # [nl, B, nh, Q, K]
-        if metric == 'gini':
+        if metric == "gini":
             batch_metric = gini_vectorized(batch_attns)
-        elif metric == 'zeros_ratio':
+        elif metric == "zeros_ratio":
             batch_metric = zeros_ratio_vectorized(batch_attns, metric_threshold)
-        elif metric == 'paibb':
+        elif metric == "paibb":
             batch_metric = paibb_vectorized(matcher, outputs, targets)
-        
+
         dataset_metric.append(batch_metric.detach().cpu())
 
         del attentions
         del conv_features
-    
+
     rank_gini = torch.stack(dataset_metric)
-    click.echo(f"Rank: {dist_config.rank}; Shape={rank_gini.shape}, Mean: {torch.mean(rank_gini, 0)}")
+    click.echo(
+        f"Rank: {dist_config.rank}; Shape={rank_gini.shape}, Mean: {torch.mean(rank_gini, 0)}"
+    )
 
     final_score = dist_utils.all_gather(rank_gini)
     final_score = torch.cat(final_score, dim=0)
@@ -163,7 +205,7 @@ def main(
             "resume_from_checkpoint": resume_from_checkpoint,
             "mean": mean,
             "std": std,
-            "decoder_act": decoder_act
+            "decoder_act": decoder_act,
         }
 
         fname = f"outputs/metrics/{decoder_act}-{metric}.pt"
