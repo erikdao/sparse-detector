4d4370a90275ae77be83114f4ba04eb351fdf6cb
erikdao <cuong@neuraltalks.io>
2022-07-24 02:04:53 +0200

    Add: detection results for all models, to include in report

--- sparse_detector/utils/box_ops.py
+++ sparse_detector/utils/box_ops.py
@@ -5,6 +5,13 @@ import torch
 from torchvision.ops.boxes import box_area
 
 
+def rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+
+
 def box_cxcywh_to_xyxy(x):
     x_c, y_c, w, h = x.unbind(-1)
     b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
